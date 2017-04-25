b039440be7f090e19f14d50c48e193983bc5307c
Alvin Tung <aytung94@gmail.com>
2017-04-25 00:23:29 -0500

    added loop for categories and renaming folders

--- run.sh
+++ run.sh
@@ -1,24 +1,56 @@
 #!/bin/bash
 
-category="accordion"
+categorylist=(
+    "accordion" 
+    "ant" 
+    "barrel" 
+    "beaver" 
+    "binoculars"     
+    "cannon" 
+    "cougar" 
+    "crayfish" 
+    "cup"
+    "dalmatian" 
+    "dragonfly"    
+    "pitcher" 
+    "flamingo"     
+    "wallaby"     
+    "laptop"
+    "leopard"
+    "llama"
+    "pizza" 
+    "platypus" 
+    "revolver" 
+    "sax"
+    "schooner" 
+    "scorpion" 
+    "strawberry" 
+    "tick" 
+    "trilobite" 
+    "umbrella" 
+)
 
-FILES="examples/SimpleLinux/101_ObjectCategories/$category/*"
-
-#for f in $FILES
-#do
-#  python  tensorflow/models/tutorials/image/imagenet/classify_image.py --image_file $f 
-#done
-
-echo "--------------------"
-
-for f in $FILES
+for category in "${categorylist[@]}"
 do
-  #echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  #echo $f
-  ./examples/SimpleLinux/deepbelief.out $f ./examples/SimpleLinux/ccv2012.ntwk $category
-done
+    #category=i
 
+    FILES="examples/SimpleLinux/101_ObjectCategories/$category/*"
+
+    #for f in $FILES
+    #do
+    #  python  tensorflow/models/tutorials/image/imagenet/classify_image.py --image_file $f 
+    #done
+
+    echo "--------------------"
+
+    for f in $FILES
+    do
+      #echo "Processing $f file..."
+      # take action on each file. $f store current file name
+      #echo $f
+      ./examples/SimpleLinux/deepbelief.out $f ./examples/SimpleLinux/ccv2012.ntwk $category
+    done
+done
 
 #for((i=1; i <= 5; i++))
 #do
